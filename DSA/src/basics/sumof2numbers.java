0486a298a0244c4be67a54331446dcfbeb842afa
Rohit-2050 <rohit.m.sde1@gmail>
2025-07-01 08:58:49 +0530

    add sumof2numbers.java

---
+++ DSA/src/basics/sumof2numbers.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class sumof2numbers {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("the sum is "+ sum);
+    }
+}
